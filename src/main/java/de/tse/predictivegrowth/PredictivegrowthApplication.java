631fabebe084e4a98f8081f7eaca59565ee599ff
tse <thomas.seine@ppi.de>
2020-10-19 18:59:10 +0200

    Initial commit

---
+++ src/main/java/de/tse/predictivegrowth/PredictivegrowthApplication.java
@@ -0,0 +1,13 @@
+package de.tse.predictivegrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictivegrowthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictivegrowthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tse/predictivegrowth/PredictivegrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tse.predictivegrowth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictivegrowthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
